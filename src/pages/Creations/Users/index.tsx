b23068383d42e9351f9f13a59b2788faa3497ec4
luyz-gusta <luizgustavo.sp2020@gmail.com>
2025-03-04 16:32:36 -0300

    Ajustando erro de componente

--- src/pages/Creations/Users/index.tsx
+++ src/pages/Creations/Users/index.tsx
@@ -1,12 +1,12 @@
 import { Container } from "../../../components/Container";
 import CreationContainer from "../../../components/CreationContainer";
 
-export default function CreateUser(){
-    return(
-        <Container>
-            <CreationContainer title="Cadastrar Usuário">
-                Teste
-            </CreationContainer>
-        </Container>
-    )
+export default function CreateUser() {
+  return (
+    <Container>
+      <CreationContainer title="Novo Usuário" subTitle="Cadastrar Usuário">
+        <h1>Users</h1>
+      </CreationContainer>
+    </Container>
+  );
 }
